d39c8c9eb014c2c0a07d8a40371bd041df6fe9f8
Nikitin Aleksandr <nikitinav@atomsbt.ru>
2025-07-04 13:18:51 +0300

    сделал pgx, Create_ctx()

--- internal/create_files/crud_tables/crud_tables_create.go
+++ internal/create_files/crud_tables/crud_tables_create.go
@@ -100,36 +100,68 @@ func CreateFiles_Create(MapAll map[string]*types.Table, Table1 *types.Table) err
 func CreateFiles_Create1(Text string, Table1 *types.Table) string {
 	Otvet := Text
 
+	MassAllColumns := micro.MassFrom_Map(Table1.MapColumns)
+
 	//Primary key
+	ReplacePKNotEqual0 := ""
+	ReplaceMassValuesIDAppend := ""
+	ReplaceTextSQLCreateWithoutID_id := ""
+	ReplaceTextSQLCreateWithoutID_values_id := ""
 	ColumnsPK := create_files.Find_PrimaryKeyColumns(Table1)
 	ReplacePKFieldsWithComma := ""
 	ReplacePKFieldNamesFormat := ""
 	ReplaceIDNot0 := ""
 	Comma := ""
+	NewLine := ""
 	TextAnd := ""
+	TextOR := ""
+	//CommaNewLine := ""
+	NumberID := len(MassAllColumns) - len(ColumnsPK)
 	for _, Column1 := range ColumnsPK {
+		NumberID = NumberID + 1
+		sNumberID := strconv.Itoa(NumberID)
 		ReplacePKFieldsWithComma = ReplacePKFieldsWithComma + Comma + "m." + Column1.NameGo
 		ReplacePKFieldNamesFormat = ReplacePKFieldNamesFormat + Comma + Column1.NameGo + ": %v"
 		TextEmpty := create_files.FindText_NotEqualEmpty(Column1, "m."+Column1.NameGo)
 		ReplaceIDNot0 = ReplaceIDNot0 + TextAnd + TextEmpty
+		ReplaceTextSQLCreateWithoutID_id = ReplaceTextSQLCreateWithoutID_id + ",\n\t" + Column1.Name
+		ReplaceTextSQLCreateWithoutID_values_id = ReplaceTextSQLCreateWithoutID_values_id + ", $" + sNumberID
+		TextNotEqual0 := create_files.FindText_NotEqualEmpty(Column1, "m."+Column1.NameGo)
+		ReplacePKNotEqual0 = ReplacePKNotEqual0 + TextOR + TextNotEqual0
+
+		TextValue := "m." + Column1.NameGo
+		if Column1.IsNullable == true {
+			TextValue = create_files.FindText_NullValue(Column1.TypeGo, TextValue)
+		}
+		ReplaceMassValuesIDAppend = ReplaceMassValuesIDAppend + NewLine + "MassValues = append(MassValues, " + TextValue + ")"
 
 		Comma = ", "
 		TextAnd = " && "
+		NewLine = "\n\t"
+		TextOR = " || "
+		//CommaNewLine = ",\n"
 	}
 	Otvet = strings.ReplaceAll(Otvet, "ReplacePKFieldsWithComma", ReplacePKFieldsWithComma)
 	Otvet = strings.ReplaceAll(Otvet, "ReplacePKFieldNamesFormat", ReplacePKFieldNamesFormat)
 	Otvet = strings.ReplaceAll(Otvet, "ReplaceIDNot0", ReplaceIDNot0)
+	Otvet = strings.ReplaceAll(Otvet, "ReplaceMassValuesIDAppend", ReplaceMassValuesIDAppend)
+	Otvet = strings.ReplaceAll(Otvet, "ReplacePKNotEqual0", ReplacePKNotEqual0)
 
 	//все колонки
+	ReplaceMassValuesAppend := ""
+	TextSQLCreateWithoutID_values := ""
+	TextSQLCreateWithoutID := ""
 	ReplaceAllFieldsWithComma := ""
 	ReplaceAllColumnNamesWithComma := ""
 	ReplaceDollarsWithComma := ""
 	Comma = ""
 	CommaNewline := ""
 	CommaNewline2 := ""
+	NewLine = ""
+	CommaNewLine := ""
 	Number := 0
-	MassColumns := micro.MassFrom_Map(Table1.MapColumns)
-	for _, Column1 := range MassColumns {
+	NumberNotID := 0
+	for _, Column1 := range MassAllColumns {
 		//кроме ненужных колонок
 		if create_files.Is_Need_Сolumn(Column1) == false {
 			continue
@@ -143,6 +175,11 @@ func CreateFiles_Create1(Text string, Table1 *types.Table) string {
 
 		ReplaceAllColumnNamesWithComma = ReplaceAllColumnNamesWithComma + CommaNewline + Column1.Name
 		ReplaceDollarsWithComma = ReplaceDollarsWithComma + Comma + "$" + sNumber
+
+		TextValue := "m." + Column1.NameGo
+		if Column1.IsNullable == true {
+			TextValue = create_files.FindText_NullValue(Column1.TypeGo, TextValue)
+		}
 		if Column1.IsPrimaryKey == true {
 			//TextValue := "m." + Column1.NameGo
 			//TextValue = create_files.FindText_NilValue(Column1.TypeGo, TextValue)
@@ -151,20 +188,39 @@ func CreateFiles_Create1(Text string, Table1 *types.Table) string {
 		} else if Column1.Name == "created_at" {
 			ReplaceAllFieldsWithComma = ReplaceAllFieldsWithComma + CommaNewline2 + "time.Now()"
 		} else {
-			TextValue := "m." + Column1.NameGo
-			if Column1.IsNullable == true {
-				TextValue = create_files.FindText_NullValue(Column1.TypeGo, TextValue)
-			}
 			ReplaceAllFieldsWithComma = ReplaceAllFieldsWithComma + CommaNewline2 + TextValue
 		}
+
+		if Column1.IsPrimaryKey == false {
+			NumberNotID = NumberNotID + 1
+			sNumberNotID := strconv.Itoa(NumberNotID)
+			TextSQLCreateWithoutID = TextSQLCreateWithoutID + CommaNewLine + Column1.Name
+			TextSQLCreateWithoutID_values = TextSQLCreateWithoutID_values + Comma + "$" + sNumberNotID
+			ReplaceMassValuesAppend = ReplaceMassValuesAppend + NewLine + "MassValues = append(MassValues, " + TextValue + ")"
+		}
+
 		Comma = ", "
 		CommaNewline = ",\n\t"
 		CommaNewline2 = ",\n\t\t"
+		NewLine = "\n\t"
+		CommaNewLine = ",\n\t"
 	}
 	Otvet = strings.ReplaceAll(Otvet, "ReplaceAllFieldsWithComma", ReplaceAllFieldsWithComma)
 	Otvet = strings.ReplaceAll(Otvet, "ReplaceAllColumnNamesWithComma", ReplaceAllColumnNamesWithComma)
 	Otvet = strings.ReplaceAll(Otvet, "ReplaceDollarsWithComma", ReplaceDollarsWithComma)
 	Otvet = strings.ReplaceAll(Otvet, "ReplaceTableName", Table1.Name)
+	Otvet = strings.ReplaceAll(Otvet, "ReplaceMassValuesAppend", ReplaceMassValuesAppend)
+
+	ReplaceTextSQLCreateWithoutID := "const TextSQL_Create_WithoutID = " +
+		"`" + "\nINSERT INTO " + Table1.Name + "(" + TextSQLCreateWithoutID + ")\n" +
+		"VALUES (" + TextSQLCreateWithoutID_values + ")\n" +
+		"`"
+	ReplaceTextSQLCreate := "const TextSQL_Create = " +
+		"`" + "\nINSERT INTO " + Table1.Name + "(" + TextSQLCreateWithoutID + ReplaceTextSQLCreateWithoutID_id + ")\n" +
+		"VALUES (" + TextSQLCreateWithoutID_values + ReplaceTextSQLCreateWithoutID_values_id + ")\n" +
+		"`"
+	Otvet = strings.ReplaceAll(Otvet, "ReplaceTextSQLCreateWithoutID", ReplaceTextSQLCreateWithoutID)
+	Otvet = strings.ReplaceAll(Otvet, "ReplaceTextSQLCreateWithID", ReplaceTextSQLCreate)
 
 	return Otvet
 }
